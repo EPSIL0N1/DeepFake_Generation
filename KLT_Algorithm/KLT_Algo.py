5009b474997b3e44fb641fee263c5aeede0e55f6
Sourik Poddar <112804344+EPSIL0N1@users.noreply.github.com>
2024-09-01 23:22:28 +0530

    second Commit

--- KLT_Algorithm/KLT_Algo.py
+++ KLT_Algorithm/KLT_Algo.py
@@ -6,22 +6,22 @@ parser.add_argument('image', type=str, help='path to image file')
 args = parser.parse_args()
 print(args)
 cap = cv.VideoCapture(args.image)
-# params for ShiTomasi corner detection
+# This is for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
                        minDistance = 7,
                        blockSize = 7 )
-# Parameters for lucas kanade optical flow
+
 lk_params = dict( winSize  = (15, 15),
                   maxLevel = 2,
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-# Create some random colors
+
 color = np.random.randint(0, 255, (100, 3))
-# Take first frame and find corners in it
+
 ret, old_frame = cap.read()
 old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
 p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-# Create a mask image for drawing purposes
+
 mask = np.zeros_like(old_frame)
 while(1):
     ret, frame = cap.read()
@@ -29,9 +29,9 @@ while(1):
         print('No frames grabbed!')
         break
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # calculate optical flow
+    # optical flow Calculations
     p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-    # Select good points
+    
     if p1 is not None:
         good_new = p1[st==1]
         good_old = p0[st==1]
@@ -47,7 +47,6 @@ while(1):
     img = cv.add(frame, mask)
 
     cv.imshow('frame', img)
-    # cv.imwrite('/KLT_Algo_Output/' + str(c) + '.jpg', img)
 
     k = cv.waitKey(30) & 0xff
     if k == 27:
